a863420b7e037c4be7d18fb6b63aebc9680e709a
Mike <maholzbach@gmail.com>
2017-04-18 21:47:53 +0200

    Added ScrollReveal.js. Neat!

---
+++ scripts.js
@@ -0,0 +1,5 @@
+window.sr = ScrollReveal();
+sr.reveal('.landing', {duration: 1200});
+sr.reveal('.burger', {origin: 'left', distance: '1000px', viewFactor: 1, duration: 1000});
+sr.reveal('.listItems', {origin: 'right', distance: '1000px', viewFactor: 1, duration: 2000 }, 200);
+sr.reveal('.grill', {duration: 1400}, 150);
