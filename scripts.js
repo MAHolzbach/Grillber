730d4b8bf9b3f2fe45b76488e3684c22a5246925
Mike <maholzbach@gmail.com>
2017-04-19 20:48:40 +0200

    Framed up grill section

--- scripts.js
+++ scripts.js
@@ -2,4 +2,4 @@ window.sr = ScrollReveal();
 sr.reveal('.landing', {duration: 1200});
 sr.reveal('.burger', {origin: 'left', distance: '1000px', viewFactor: 1, duration: 1000});
 sr.reveal('.listItems', {origin: 'right', distance: '1000px', viewFactor: 1, duration: 2000 }, 200);
-sr.reveal('.grill', {duration: 1400}, 150);
+sr.reveal('.picColumn', {duration: 1000});
