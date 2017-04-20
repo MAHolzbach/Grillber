b5d34af013ee36302c60a09ef65f0df992cefe4b
Mike <maholzbach@gmail.com>
2017-04-20 12:17:19 +0200

    Grill section fleshed out, booking calendar started

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
+$(function() {
+    $("#booker").datepicker();
+  });
+
 window.sr = ScrollReveal();
 sr.reveal('.landing', {duration: 1200});
 sr.reveal('.burger', {origin: 'left', distance: '1000px', viewFactor: 1, duration: 1000});
 sr.reveal('.listItems', {origin: 'right', distance: '1000px', viewFactor: 1, duration: 2000 }, 200);
-sr.reveal('.picColumn', {duration: 1000});
+sr.reveal('.picColumn', {origin: 'left', distance: '100px', duration: 1000});
+sr.reveal('#booker', {duration: 500}, 50);
